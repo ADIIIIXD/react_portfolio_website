61fcf3d4ba2a2c3afaeb0ca644f8367f383b8203
aditya <addie4120@gmail.com>
2024-03-20 00:11:20 +0530

    added link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,12 +22,14 @@ function Home() {
           creating exceptional digital experiences.
         </p>
         <div>
-          <button className="text-white group border-2 rounded-lg px-6 py-3 my-2 flex items-center hover:bg-pink-600">
-            View Work
-            <span className="group-hover:rotate-90 duration-300">
-              <HiArrowNarrowRight className="ml-3 " />
-            </span>
-          </button>
+          <Link to="work" smooth={true} duration={500}>
+            <button className="text-white group border-2 rounded-lg px-6 py-3 my-2 flex items-center hover:bg-pink-600">
+              View Work
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowNarrowRight className="ml-3 " />
+              </span>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
